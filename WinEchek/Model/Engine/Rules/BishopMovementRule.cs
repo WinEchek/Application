ca2408014df53ab0e30a373a3f5b4b5f780a4b3b
Nexyll <antoine7104@gmail.com>
2016-11-22 00:59:28 +0100

    Règle de déplacement pour le Bishop (mauvais namespace)

--- WinEchek/Model/Engine/Rules/BishopMovementRule.cs
+++ WinEchek/Model/Engine/Rules/BishopMovementRule.cs
@@ -1,4 +1,6 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using WinEchek.Model;
 
 namespace WinEchek.Engine.Rules
@@ -7,12 +9,90 @@ namespace WinEchek.Engine.Rules
     {
         public bool IsMoveValid(Move move)
         {
-            throw new System.NotImplementedException();
+            List<Square> diagonalUpLeft = new List<Square>();
+            List<Square> diagonalUpRight = new List<Square>();
+            List<Square> diagonalDownLeft = new List<Square>();
+            List<Square> diagonalDownRight = new List<Square>();
+
+            bool diagonalUpLeftEnd = false;
+            bool diagonalUpRightEnd = false;
+            bool diagonalDownLeftEnd = false;
+            bool diagonalDownRightEnd = false;
+
+
+
+            Board board = move.Piece.Square.Board;
+
+            for (int i = 1; i < 8; i++)
+            {
+                if (move.Piece.Square.X - i < 8 && move.Piece.Square.X - i >= 0 && move.Piece.Square.Y - i < 8 &&
+                    move.Piece.Square.Y - i >= 0 && !diagonalUpLeftEnd)
+                {
+                    Square square = board.Squares[move.Piece.Square.X - i, move.Piece.Square.Y - i];
+                    diagonalUpLeft.Add(square);
+                    if (square.Piece != null)
+                    {
+                        if (move.Piece.Color == square.Piece.Color)
+                        {
+                            diagonalUpLeft.Remove(square);
+                            // on supprime si la case contient une pièce de la même couleur que celle concernée par le déplacement
+                        }
+                        diagonalUpLeftEnd = true;
+                    }
+                }
+                if (move.Piece.Square.X + i < 8 && move.Piece.Square.X + i >= 0 && move.Piece.Square.Y - i < 8 &&
+                    move.Piece.Square.Y - i >= 0 && !diagonalUpRightEnd)
+                {
+                    Square square = board.Squares[move.Piece.Square.X + i, move.Piece.Square.Y - i];
+                    diagonalUpRight.Add(square);
+                    if (square.Piece != null)
+                    {
+                        if (move.Piece.Color == square.Piece.Color)
+                        {
+                            diagonalUpRight.Remove(square);
+                            // on supprime si la case contient une pièce de la même couleur que celle concernée par le déplacement
+                        }
+                        diagonalUpRightEnd = true;
+                    }
+                }
+                if (move.Piece.Square.X - i < 8 && move.Piece.Square.X - i >= 0 && move.Piece.Square.Y + i < 8 &&
+                    move.Piece.Square.Y + i >= 0 && !diagonalDownLeftEnd)
+                {
+                    Square square = board.Squares[move.Piece.Square.X - i, move.Piece.Square.Y + i];
+                    diagonalDownLeft.Add(square);
+                    if (square.Piece != null)
+                    {
+                        if (move.Piece.Color == square.Piece.Color)
+                        {
+                            diagonalDownLeft.Remove(square);
+                            // on supprime si la case contient une pièce de la même couleur que celle concernée par le déplacement
+                        }
+                        diagonalDownLeftEnd = true;
+                    }
+                }
+                if (move.Piece.Square.X + i < 8 && move.Piece.Square.X + i >= 0 && move.Piece.Square.Y + i < 8 &&
+                    move.Piece.Square.Y + i >= 0 && !diagonalDownRightEnd)
+                {
+                    Square square = board.Squares[move.Piece.Square.X + i, move.Piece.Square.Y + i];
+                    diagonalDownRight.Add(square);
+                    if (square.Piece != null)
+                    {
+                        if (move.Piece.Color == square.Piece.Color)
+                        {
+                            diagonalDownRight.Remove(square);
+                            // on supprime si la case contient une pièce de la même couleur que celle concernée par le déplacement
+                        }
+                        diagonalDownRightEnd = true;
+                    }
+                }
+            }
+
+            List<Square> possibleSquares = diagonalUpLeft.Concat(diagonalUpRight)
+                                                         .Concat(diagonalDownLeft)
+                                                         .Concat(diagonalDownRight)
+                                                         .ToList();
+            return possibleSquares.Contains(move.Square);
         }
 
-        private bool isInDiagonal(int x, int y, int X, int Y)
-        {
-            return true;
-        }
     }
 }
