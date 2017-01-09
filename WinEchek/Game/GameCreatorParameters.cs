e3b2664fa4d240ff0c36820f02ef5c274f39f594
Nexyll <antoine7104@gmail.com>
2017-01-09 23:20:39 +0100

    Important commit

---
+++ WinEchek/Game/GameCreatorParameters.cs
@@ -0,0 +1,11 @@
+﻿namespace WinEchek.Game
+{
+    public class GameCreatorParameters
+    {
+        public int AiSkillLevel { get; set; }
+        public string AiSearchType { get; set; }
+        public int AiSearchValue { get; set; }
+
+        //TODO add player color parameter
+    }
+}
